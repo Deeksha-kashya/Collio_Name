b5a86243e0120dfecef1d1bedcba461a9058c448
dell <dell@192.168.5.24>
2023-07-09 12:49:44 -0400

    second comment

---
+++ Projects/src/Calucater.java
@@ -0,0 +1,17 @@
+
+public class Calucater {
+	
+	public int add(int a, int b) {
+		int sum = a+b;
+		 return sum;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calucater calucater= new Calucater();
+		calucater.add(55, 89);
+
+	}
+
+}
